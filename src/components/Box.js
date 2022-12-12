ef69a0dd9e7bf4f440fb35b1ddb8f1c27b7d49dd
tomas oyarzun <xilent312@gmail.com>
2022-12-12 14:42:46 -0300

    automove geometry

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,14 +3,19 @@ import texture from '../image/JavaScript_Logo.webp'
 import {TextureLoader} from 'three/src/loaders/TextureLoader'
 import React from 'react'
 import { RoundedBox } from '@react-three/drei'
+import * as THREE from 'three'
 
 const Box = () => {
-
+    const repeatX = 1
+    const repeatY = 1
     const colorMap = useLoader( TextureLoader ,texture)
+    colorMap.wrapS = THREE.RepeatWrapping 
+    colorMap.wrapT = THREE.RepeatWrapping
+    colorMap.repeat.set(repeatX,repeatY)
   return (
-    <mesh>
+    <mesh scale={1.0}>
         <boxBufferGeometry attach='geometry' args={[3,3,3]}></boxBufferGeometry>
-        <meshStandardMaterial map={colorMap}/>
+        <meshPhysicalMaterial map={colorMap}/>
 
         <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
   <meshPhongMaterial color="#f3f3f3" wireframe />
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,18 +1,37 @@
 import React from 'react'
 import style from '../styles/Landing.module.css'
-import {Canvas} from '@react-three/fiber'
+import {Canvas, extend} from '@react-three/fiber'
+import { Cloud, Effects, Extrude, Sparkles } from '@react-three/drei'
+import {UnrealBloomPass} from 'three-stdlib'
 import Box from './Box'
 import { OrbitControls, Stars } from '@react-three/drei'
-const Landing = () => {
 
+extend({UnrealBloomPass})
+const Landing = () => {
+ 
   return (
     <div id='inicio' className={style.container}>
-       <Canvas>
-          <OrbitControls/>
+       <Canvas shadows gl={{antialias : false}}>
+          <color attach='background' args={['#202030']}/>
+          <OrbitControls autoRotate enableZoom={false}/>
           <Stars/>
           <ambientLight intensity={0.5}/>
           <spotLight position={[10,15,10]} angle={0.3}/>
           <Box/>
+          <hemisphereLight intensity={0} color={'red'}  />
+          <Sparkles count={200} scale={[20,20,10]} size={1} speed={0.01}/>
+          <fog attach={'fog'} args={['#202030',5,25]}/>
+          <Cloud
+          opacity = {0.1}
+          speed={0.1}
+          width={30}
+          depth={0.1}
+          segments={40}
+          />
+
+          <Effects disableGamma>
+            <unrealBloomPass threshold={1} strength={2} radius={2.0} />
+          </Effects> 
         </Canvas>
 
        <div className={style.sub_container}>
